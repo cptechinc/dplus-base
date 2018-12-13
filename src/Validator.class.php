bba73cdc52843c248b93189f47b7574aa39e9d3d
Paul Gomez <pauldro952@outlook.com>
2018-12-13 09:40:09 -0600

    Added 10 digit phone validator

--- src/Validator.class.php
+++ src/Validator.class.php
@@ -7,7 +7,7 @@
     class Validator {
 
         /**
-         * Validate that value is an integer, float/decimal, boolean, or string. Value can be zero (0) or FALSE.
+         * Validate that value is an integer, float/decimal, bool, or string. Value can be zero (0) or FALSE.
          * @param    string    $var             Value to validate
          * @return   bool
          */
@@ -19,7 +19,7 @@
          * Validate that value is an integer whether in string or not (numberic only).
          * 
          * @param    mixed    $var                Value to validate
-         * @return   boolean
+         * @return   bool
          */
         public function integer($var) {
             return (is_int($var) || (is_string($var) && ctype_digit($var)));
@@ -73,7 +73,7 @@
          * Validate that value is a date string in the YYYYMMDD HH:MM:SS format
          * 
          * @param    mixed    $var                Value to validate
-         * @return   boolean
+         * @return   bool
          */
         public function date_yyyymmdd_hhmmss($var) {
             return (empty($var) || preg_match('~^\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}$~', $var) === 1);
@@ -152,7 +152,7 @@
          * 
          * @param    mixed     $max                Maximum value
          * @param    mixed     $var
-         * @return   boolean
+         * @return   bool
          */
         public function max_value($max, $var) {
             if (is_string($max)) {
@@ -168,7 +168,7 @@
          * 
          * @param    mixed     $min                Minimum value
          * @param    mixed     $var
-         * @return   boolean
+         * @return   bool
          */
         public function min_value($min, $var) {
             if (is_string($min)) {
@@ -183,7 +183,7 @@
          * // NOTE Strings are converted to integers/floats.
          * @param    mixed     $exact        Exact value
          * @param    string    $var                
-         * @return   boolean
+         * @return   bool
          */
         public function exact_value($exact, $var) {
             if (is_string($exact)) {
@@ -191,5 +191,15 @@
             }
     
             return ($$var == $exact);
+        }
+
+        /**
+         * Returns if string matches 10 digit phone number
+         * @param  string $str String to Match Phone Number against
+         * @return bool        Does string match regex for 10 digit phone number?
+         */
+        public function phone_10($str) {
+            $regex = "/\d{3}-?\d{3}-?\d{4}/i";
+			return preg_match($regex, $str) ? true : false;
         }
     }
